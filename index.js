094d4604a4317b0f4b042478b02555a5e777e0bb
niikofi <163415241+niikofi@users.noreply.github.com>
2024-06-29 20:50:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+/*TASK ONE (A)
+(Declaration and Initialization of Variable)*/
+let name = 'William';
+let age = 65;
+let isStudent = false;
+
+/*(B)
+ Logging and indicating type of value*/
+console.log("Age:",age,"Type:",typeof age);
+console.log("Name:",name, "Type:",typeof name);
+console.log("Is Student:", isStudent,"Type:",typeof isStudent);
+
+
+/*TASK2 TWO
+Declaration of Values*/
+
+let a = 27;
+let b = 61;
+
+//DECLARATION OF ARITHMETIC OPERATIONS
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+//RESULTS OF ARITHMETIC OPERATIONS
+console.log("Addition:",addition);
+console.log("Subtraction:",subtraction);
+console.log("Multiplication:",multiplication);
+console.log("Division:",division);
+
+
+//TASK THREE
+
+//Declaration of values
+
+let x = 28;
+let y = "15";
+
+//COMPARISON OPERATORS
+console.log("x==y:",x==y);
+console.log("x===y:",x===y);
+console.log("x!=y",x!=y);
+console.log("x!==y:",x!==y);
+console.log("x > y:", x > y);
+console.log("x < y:", x < y);
+console.log("x>=y:",x>=y);
+console.log("x<=y:",x<=y);
+
+//TASK FOUR
+//Declaration of values
+
+numberString = '14';
+actualNumber = 8;
+
+//Demonstration of coercion
+console.log("Addition:", numberString + actualNumber);
+console.log("Subtraction:",numberString - actualNumber);
+console.log("Multiplication:", numberString * actualNumber);
+console.log("Division:",numberString / actualNumber );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
